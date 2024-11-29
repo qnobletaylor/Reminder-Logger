23baf3762583a86bcbec85c0b1aabcd08aa34069
Quinlin Taylor <102311011+qnobletaylor@users.noreply.github.com>
2024-11-29 17:14:50 -0500

    started making the servlet in HelloServlet

--- src/main/java/edu/bhcc/HelloServlet.java
+++ src/main/java/edu/bhcc/HelloServlet.java
@@ -9,6 +9,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,6 +20,8 @@ import java.util.Map;
  * Hello, World Servlet.
  */
 public class HelloServlet extends HttpServlet {
+  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
+  private Connection database;
 
   /**
    * Process an HTTP Request.
@@ -25,22 +31,58 @@ public class HelloServlet extends HttpServlet {
     response.setContentType("text/html");
     response.setStatus(HttpServletResponse.SC_OK);
 
-    // Create the Model
-    Map<String, Object> root = new HashMap<>();
-    root.put("user", "Quinlin");
-
-    // Get the FreeMarker Template Engine
-    FreeMarkerUtil setup = FreeMarkerUtil.getInstance();
-    Configuration cfg = setup.getFreeMarkerConfiguration();
-    Template template = cfg.getTemplate("hello.html");
-
-    //  Merge the Model with the Template
-    PrintWriter writer = response.getWriter();
-
     try {
-      template.process(root, writer);
-    } catch (TemplateException e) {
-      writer.println("Could not process template:  " + e.getMessage());
+      database = DriverManager.getConnection("jdbc:sqlite:src/main/webapp/WEB-INF/log.db");
+      
+      // Create the Model
+      Map<String, Object> root = new HashMap<>();
+      root.put("msgList", getMessages(database, request));
+
+      // Get the FreeMarker Template Engine
+      FreeMarkerUtil setup = FreeMarkerUtil.getInstance();
+      Configuration cfg = setup.getFreeMarkerConfiguration();
+      Template template = cfg.getTemplate("hello.html");
+
+      //  Merge the Model with the Template
+      PrintWriter writer = response.getWriter();
+
+      try {
+        template.process(root, writer);
+      } catch (TemplateException e) {
+        writer.println("Could not process template:  " + e.getMessage());
+      }
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
+
+
+  }
+
+  private void newMessage(Connection connection, HttpServletRequest request) throws SQLException {
+    PreparedStatement ps = connection.prepareStatement("INSERT INTO log VALUES(?, ?)");
+    LocalTime now = LocalTime.now();
+    String time = now.format(formatter);
+
+    ps.setString(1, time);
+    ps.setString(2, request.getParameter("msg"));
+  }
+
+  private ArrayList<LogMsg> getMessages(Connection connection, HttpServletRequest request) throws SQLException {
+    ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM log");
+    ArrayList<LogMsg> messages = new ArrayList<>();
+
+    if (resultSet.next()) {
+      do {
+        String time = resultSet.getString("TIME");
+        String msg = resultSet.getString("MESSAGE");
+
+        messages.add(new LogMsg(time, msg));
+      } while (resultSet.next());
+    }
+    return messages;
+  }
+
+  private record LogMsg(String time, String message) {
   }
 }
